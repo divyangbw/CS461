ed5fa48ab019fa5d0c0fc8607b05e947776eb6f9
Kyle Schilling <schilkyl@gmail.com>
2014-12-06 17:10:34 -0600

    fix recent additions

--- finders/models/assignments.py
+++ finders/models/assignments.py
@@ -3,13 +3,13 @@ from finders.models import ser
 
 from finders import db
 
-class Assignments(db.Model):
-    __tablename__ = 'assignments'
+class Assignment(db.Model):
+    __tablename__ = 'assignment'
     id = db.Column(db.Integer, primary_key=True)
     seg_id = db.Column(db.Integer)
     user_id = db.Column(db.Integer)
-    answers = db.relationship('Option', backref='option',lazy='dynamic') #select, joined, subquery
-    section = db.relationship('Option', backref='option',lazy='dynamic') #select, joined, subquery
+    answers = db.relationship('Answer', backref='answer',lazy='dynamic') #select, joined, subquery
+    sections = db.relationship('AssignSection', backref='assignsection',lazy='dynamic') #select, joined, subquery
     updated = db.Column(db.DATETIME)
 
     @property
@@ -18,26 +18,26 @@ class Assignments(db.Model):
            'id': self.id,
            'seg_id' : self.seg_id,
            'user_id' : self.user_id,
-           'answers'  : self.serialize_many2many,
-           'section'  : self.serialize_many2many,
+           'answers'  : self.serialize_answers,
+           'section'  : self.serialize_sections,
            'updated': ser.dump_datetime(self.updated)
        }
 
     @property
-    def serialize_many2many(self):
+    def serialize_answers(self):
        return [ item.serialize for item in self.answers ]
 
     @property
-    def serialize_many2many(self):
-       return [ item.serialize for item in self.section ]
+    def serialize_sections(self):
+       return [ item.serialize for item in self.sections ]
 
 
-class Answers(db.Model):
-    __tablename__ = 'answers'
+class Answer(db.Model):
+    __tablename__ = 'answer'
     id = db.Column(db.Integer, primary_key=True)
     question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
     answer = db.Column(db.text)
-    seg_que_id = db.Column(db.Integer)
+    assignment_id = db.Column(db.Integer)
     updated = db.Column(db.DATETIME)
 
     @property
@@ -46,20 +46,22 @@ class Answers(db.Model):
             'id': self.id,
             'question_id' : self.question_id,
             'answer' : self.answer,
-            'seg_que_id' : self.seg_que_id,
+            'assignment_id' : self.assignment_id,
             'updated' : ser.dump_datetime(self.updated)
         }
 
-class Section(db.Model):
-    __tablename__ = 'section'
+class AssignSection(db.Model):
+    __tablename__ = 'assignsection'
     id = db.Column(db.Integer, primary_key=True)
-
+    assignment_id = db.Column(db.Integer)
+    section = db.Column(db.Integer)
     updated = db.Column(db.DATETIME)
 
     @property
     def serialize(self):
-        return {
-            'id': self.id,
-
-            'updated' : ser.dump_datetime(self.updated)
-        }
+       return {
+           'id': self.id,
+           'assignment_id': self.assignment_id,
+           'section': self.section,
+           'updated': ser.dump_datetime(self.updated)
+       }
