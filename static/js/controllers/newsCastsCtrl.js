bfb8a5fe10af9d43e901a843d3a96794116de8ef
duitsman <duitsman@iastate.edu>
2014-12-07 16:18:14 -0600

    Added News Cast and Segment Functionality
    Creating and Editing News Casts works
    Creating Segment doesn't save and neither delete works

--- static/js/controllers/newsCastsCtrl.js
+++ static/js/controllers/newsCastsCtrl.js
@@ -1,40 +1,145 @@
-angular.module('reports.controllers').controller('NewsCastsCtrl', function ($scope, $state, DataFactory, User) {
+angular.module('reports.controllers').controller('NewsCastsCtrl', function ($scope, $state, DataFactory, $ionicPopup, $ionicModal, User) {
 
     $scope.loadingData = true;
 
     $scope.newsCasts = {};
     $scope.castSegments = {};
+    $scope.form = {};
 
-    $scope.goToSegments = function(item) {
-        var cast = item;
-        DataFactory.setActiveCast(cast);
+    $scope.goToSegments = function (item) {
+        DataFactory.setActiveCast(item);
 
-        DataFactory.getSegments().then(function (res){
+        DataFactory.getSegments().then(function (res) {
             $scope.castSegments = res;
         }, function (err) {
             console.log("Error");
         });
         $state.go('tab.segments');
 
+    };
 
 
-    }
+    $scope.saveCast = function (item) {
+        DataFactory.createCast(item).then(function (response) {
+            $scope.createEditModal.hide();
+            $scope.cast = {};
+        }, function (err) {
+
+        });
+
+    };
+
+    $scope.deleteCast = function () {
+        // An elaborate, custom popup
+        var temp = $scope.cast;
+        DataFactory.deleteCast(temp).then(function (response) {
+            //$scope.createEditModal.close();
+            $scope.cast = {};
+        }, function (err) {
+
+        });
+        console.log($scope.cast);
+    };
 
 
-    DataFactory.getCasts().then(function (result){
+    DataFactory.getCasts().then(function (result) {
         $scope.newsCasts = result;
         //$scope.loadingData = true;
     }, function (err) {
         console.log("Error");
+
+
     });
+
+
+    $ionicModal.fromTemplateUrl('newCast.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.createEditModal = modal;
+    });
+    // When the new question button is clicked
+    $scope.newCast = function () {
+        // Show the modal
+        $scope.isEditing = false;
+        $scope.form = {};
+        $scope.createEditModal.show();
+    }
 
 });
 
-angular.module('reports.controllers').controller('SegmentsCtrl', function ($scope, DataFactory, User) {
-    DataFactory.getSegments().then(function (result){
+angular.module('reports.controllers').controller('SegmentsCtrl', function ($scope, DataFactory, $ionicPopup, $ionicModal, User) {
+
+    $scope.activeCast = DataFactory.getActiveCast();
+    console.log($scope.activeCast)
+    DataFactory.getSegments().then(function (result) {
         $scope.castSegments = result;
     }, function (err) {
         console.log("Error")
     })
+
+    $ionicModal.fromTemplateUrl('editCast.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.createEditModal = modal;
+    });
+    $ionicModal.fromTemplateUrl('editSegment.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.createEditSegModal = modal;
+    });
+
+
+    $scope.editSegment = function (item) {
+        // Set the editItem to what we want to edit. That way we can pre-fill the form.
+        $scope.isEditing = true;
+        $scope.editItem = item;
+        // Show the modal
+        $scope.createEditModal.show();
+    }
+    $scope.saveSeg = function (item) {
+        DataFactory.createSegment(item).then(function (response) {
+            $scope.createEditSegModal.hide();
+            $scope.segments = {};
+        }, function (err) {
+
+        });
+
+    };
+
+    $scope.deleteSegment = function () {
+        // An elaborate, custom popup
+        var temp = $scope.cast;
+        DataFactory.deleteCast(temp).then(function (response) {
+            //$scope.createEditModal.close();
+            $scope.cast = {};
+        }, function (err) {
+
+        });
+        console.log($scope.cast);
+
+    };
+    $scope.updateCast = function (item) {
+        // An elaborate, custom popup
+        DataFactory.updateCast(item).then(function (response) {
+            $scope.createEditModal.hide();
+            $scope.cast = {};
+        }, function (err) {
+
+        });
+        console.log($scope.cast);
+
+    };
+    $scope.editCast = function (item) {
+        $scope.form = item;
+        // Show the modal
+        $scope.createEditModal.show();
+    }
+    //When the add button is clicked
+    $scope.newSegment = function () {
+        // Show the modal
+        $scope.isEditing = false;
+        $scope.seg = {};
+        $scope.createEditSegModal.show();
+    }
 
 });
--- static/js/factory/dataFactory.js
+++ static/js/factory/dataFactory.js
@@ -3,19 +3,23 @@ angular.module('reports.factory')
     .factory('DataFactory', function ($q, ApiService) {
 
         var casts = [];
-        var activeCast = null;
+        var activeCast = getActiveCastStorage();
         var segments = [];
 
+        function getActiveCastStorage() {
+            var item = localStorage.getItem("activeCast");
+            return item ? JSON.parse(item) : {};
+        }
 
         return {
 
             getCasts: function () {
 
                 var deferred = $q.defer();
-                ApiService.getAllCasts().then(function (result){
+                ApiService.getAllCasts().then(function (result) {
                     casts = result;
                     deferred.resolve(casts);
-                }, function (err){
+                }, function (err) {
                     deferred.reject(err);
                 });
                 return deferred.promise;
@@ -24,21 +28,114 @@ angular.module('reports.factory')
             getSegments: function () {
 
                 var deferred = $q.defer();
-                ApiService.getAllSegments(activeCast.id).then(function (result){
+                ApiService.getAllSegments(activeCast.id).then(function (result) {
                     segments = result;
                     deferred.resolve(segments);
-                }, function (err){
+                }, function (err) {
                     deferred.reject(err);
                 });
                 return deferred.promise;
             },
 
-            getActiveCast: function (){
+            getActiveCast: function () {
+                console.log(activeCast)
                 return activeCast;
             },
 
-            setActiveCast: function(cast){
+            setActiveCast: function (cast) {
                 activeCast = cast;
+                localStorage.setItem("activeCast", JSON.stringify(activeCast));
+            },
+
+            createCast: function (item) {
+
+                var deferred = $q.defer();
+                ApiService.createCast(item).then(function (result) {
+                    casts = result;
+                    deferred.resolve(casts);
+                }, function (err) {
+                    deferred.reject(err);
+                });
+                return deferred.promise;
+
+            },
+            updateCast: function (item) {
+
+                var deferred = $q.defer();
+                ApiService.updateCast(item).then(function (result) {
+                    casts = result;
+                    deferred.resolve(casts);
+                }, function (err) {
+                    deferred.reject(err);
+                });
+                return deferred.promise;
+
+            },
+            deleteCast: function (item) {
+
+                var deferred = $q.defer();
+                ApiService.deleteCast(item).then(function (result) {
+                    casts = result;
+                    deferred.resolve(casts);
+                }, function (err) {
+                    deferred.reject(err);
+                });
+                return deferred.promise;
+
+                var index = -1;
+                for (var i = 0; i < casts.length; i++) {
+                    if (casts[i].id === item.id) {
+                        index = i;
+                        break;
+                    }
+                }
+
+                casts.splice(index, 1);
+            },
+            createSegment: function (item) {
+
+                var deferred = $q.defer();
+                ApiService.createSeg(item).then(function (result) {
+                    segments = result;
+                    deferred.resolve(segments);
+                }, function (err) {
+                    deferred.reject(err);
+                });
+                return deferred.promise;
+
+            },
+            updateSegment: function (item) {
+
+                var deferred = $q.defer();
+                ApiService.updateSeg(item).then(function (result) {
+                    segments = result;
+                    deferred.resolve(segments);
+                }, function (err) {
+                    deferred.reject(err);
+                });
+                return deferred.promise;
+
+            },
+            deleteSegment: function (item) {
+
+                var deferred = $q.defer();
+                ApiService.deleteSeg(item).then(function (result) {
+                    segments = result;
+                    deferred.resolve(segments);
+                }, function (err) {
+                    deferred.reject(err);
+                });
+                return deferred.promise;
+
+                var index = -1;
+                for (var i = 0; i < segments.length; i++) {
+                    if (segments[i].id === item.id) {
+                        index = i;
+                        break;
+                    }
+                }
+
+                segments.splice(index, 1);
             }
 
 
