5fb1484bdabd140c43de8fac0dc9a2440080546b
Kyle Schilling <schilkyl@gmail.com>
2014-12-09 17:54:56 -0600

    add main foundation for displaying results

--- finders/static/js/controllers/resultsCtrl.js
+++ finders/static/js/controllers/resultsCtrl.js
@@ -1,5 +1,38 @@
-angular.module('reports.controllers').controller('ResultsCtrl', function ($scope) {
+angular.module('reports.controllers').controller('ResultsCtrl', function ($scope, $state, DataFactory, $ionicModal) {
+
+    $scope.loadingData = true;
+
+    $scope.newsCasts = {};
+    $scope.castSegments = {};
+
+    $scope.goToSegments = function (item) {
+        DataFactory.setActiveCast(item);
+
+        DataFactory.getSegments().then(function (res) {
+            $scope.castSegments = res;
+        }, function (err) {
+            console.log("Error");
+        });
+        $state.go('tab.segments');
+    };
+
+    DataFactory.getCasts().then(function (result) {
+        $scope.newsCasts = result;
+        //$scope.loadingData = true;
+    }, function (err) {
+        console.log("Error");
 
 
+    });
+});
 
+angular.module('reports.controllers').controller('SegmentsCtrl', function ($scope, DataFactory, $ionicPopup, $ionicModal, User) {
+
+    $scope.activeCast = DataFactory.getActiveCast();
+    console.log($scope.activeCast)
+    DataFactory.getSegments().then(function (result) {
+        $scope.castSegments = result;
+    }, function (err) {
+        console.log("Error")
+    })
 });
