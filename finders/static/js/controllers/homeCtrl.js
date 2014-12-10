b1cb06c0daea224fc4a9a7664857c3237c43954c
Divyang Joshi <divyangbw@live.com>
2014-12-10 14:15:13 -0600

    Added search to home page

--- finders/static/js/controllers/homeCtrl.js
+++ finders/static/js/controllers/homeCtrl.js
@@ -1,6 +1,7 @@
 angular.module('reports.controllers')
     .controller('HomeCtrl', function ($scope, $state, $ionicViewService, $ionicPopover, User, TempDataFactory) {
 
+        $scope.filter = {};
         $scope.incomplete = [];
         $scope.toStart = [];
         $scope.completed = [];
@@ -18,8 +19,65 @@ angular.module('reports.controllers')
                 $scope.completed = TempDataFactory.getCompletedAssignments();
                 $scope.incomplete = TempDataFactory.getIncompleteAssignments();
                 $scope.toStart = TempDataFactory.getNotStartedAssignments();
-                console.log($scope.toStart)
+                $scope.completed.forEach(function (item) {
+                    item.isVisible = true;
+                });
+                $scope.incomplete.forEach(function (item) {
+                    item.isVisible = true;
+                });
+                $scope.toStart.forEach(function (item) {
+                    item.isVisible = true;
+                });
             });
+        }
+
+        $scope.$watch('filter.search', function (newVal, oldVal) {
+            if (!newVal || newVal.length === 0) {
+                $scope.completed.forEach(function (item) {
+                    item.isVisible = true;
+                });
+                $scope.incomplete.forEach(function (item) {
+                    item.isVisible = true;
+                });
+                $scope.toStart.forEach(function (item) {
+                    item.isVisible = true;
+                });
+            }
+            if (newVal != oldVal) {
+                $scope.completed.forEach(function (item) {
+                    searchItem(item, newVal)
+                });
+                $scope.incomplete.forEach(function (item) {
+                    searchItem(item, newVal)
+                });
+                $scope.toStart.forEach(function (item) {
+                    searchItem(item, newVal)
+                });
+            }
+        });
+
+        function searchItem(item, newVal) {
+            //User
+            if (item.user.first.indexOf(newVal) > -1 || item.user.last.indexOf(newVal) > -1
+                || item.user.email.indexOf(newVal) > -1) {
+                item.isVisible = true;
+                return;
+            }
+
+            //segment
+            if (item.segment.subject.indexOf(newVal) > -1 || item.segment.comment.indexOf(newVal) > -1) {
+                item.isVisible = true;
+                return;
+            }
+
+            //cast
+            if (item.cast.company.indexOf(newVal) > -1 || item.cast.date[0].indexOf(newVal) > -1) {
+                item.isVisible = true;
+                return;
+            }
+
+            item.isVisible = false;
+            return;
         }
 
     });
