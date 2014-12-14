414d656de115f54d75bf3d29b948a11b4d69bc4d
Kyle Schilling <schilkyl@gmail.com>
2014-12-13 22:57:26 -0600

    remove logs and add some comments

--- finders/static/js/controllers/resultsCtrl.js
+++ finders/static/js/controllers/resultsCtrl.js
@@ -1,3 +1,4 @@
+// first controller for results page
 angular.module('reports.controllers').controller('ResultsCtrl', function ($scope, $state, DataFactory, $ionicModal, TempDataFactory) {
 
     // completed assignments
@@ -5,32 +6,37 @@ angular.module('reports.controllers').controller('ResultsCtrl', function ($scope
     // answers to a specific assignment
     $scope.answers = [];
 
+    // gather required data. can be expanded with more functionality if necessary
     getData();
 
     function getData() {
         TempDataFactory.refreshMyAssignments(false).then(function () {
             $scope.completed = TempDataFactory.getCompletedAssignments();
-            console.log($scope.completed);
         });
     }
 
+    // function to go to results for a select section
     $scope.goToAnswers = function (item) {
-        //Set the active answer to the answer that is being displayed
+        // save active answer so that it can be displayed on results page
         DataFactory.setActiveAnswer(item);
-        //Get the answers of the item
+        // gather results of selected section
         DataFactory.getAnswersForSegment().then(function (result) {
             $scope.answers = result;
         }, function (err) {
             console.log(err);
         });
+        // navigate to answers page
         $state.go('tab.answers');
     };
 });
 
+// second controller for answers page
 angular.module('reports.controllers').controller('ResultsAnswerCtrl', function ($scope, $state, DataFactory, $ionicModal, TempDataFactory) {
 
+    // retrieve stored segment that was clicked on to get here
     $scope.activeAnswer = DataFactory.getActiveAnswer();
 
+    // gather results of selected section
     DataFactory.getAnswersForSegment().then(function (result) {
         $scope.answers = result;
     }, function (err) {
